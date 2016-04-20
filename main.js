e617e517e393075fd0386ecf122d028fbff56238
Erika Perugachi <erikaperugachi@gmail.com>
2016-04-20 11:53:31 -0500

    add redux

--- main.js
+++ main.js
@@ -1,84 +1,18 @@
 import React, { Component } from 'react'
-import {render} from 'react-dom';
+import ReactDOM from 'react-dom';
 import MonkeyUI from './components/MonkeyUI.js';
-//import monkey from './src/monkey.js'
+import monkey from './src/monkey.js'
+
+import {createStore} from 'redux';
+import messages from './reducers';
+
+const store = createStore(messages);
 
 class App extends React.Component {
 	constructor(props){
 		super(props);
 		this.state = {
-			conversations: {
-				'idlk0p519nvfmfgfzdbfn7b9': {
-					id: 'idlk0p519nvfmfgfzdbfn7b9',
-					lastMessage: undefined,
-					members: undefined,
-					name: 'Alberto',
-					unreadMessageCount: 0,
-					urlAvatar: 'https://secure.criptext.com/avatars/avatar_707.png',
-					messages: {
-						'1': {
-							id: '1',
-							senderId: 'if9ynf7looscygpvakhxs9k9',
-							timestamp: 2,
-							text: 'mensaje 1',
-							recipientId: 'idlk0p519nvfmfgfzdbfn7b9',
-							status: 0,
-							type: 2,
-							data: 'http://www.mejoresjugosparabajardepeso.com/wp-content/uploads/2015/08/manzana-verde-3-300x198.jpg',
-						},
-						'2': {
-							id: '2',
-							senderId: 'idlk0p519nvfmfgfzdbfn7b9',
-							timestamp: 2,
-							text: 'mensaje 2',
-							recipientId: 'if9ynf7looscygpvakhxs9k9',
-							status: 50,
-							type: 1,
-						}
-					}
-				},
-				'ife4c0qdb0dopbg538lg14i': {
-					id: 'ife4c0qdb0dopbg538lg14i',
-					lastMessage: undefined,
-					members: undefined,
-					name: 'Luis Loaiza',
-					unreadMessageCount: 0,
-					urlAvatar: 'https://secure.criptext.com/avatars/avatar_62.png',
-					messages: {
-						'1': {
-							id: '1',
-							senderId: 'if9ynf7looscygpvakhxs9k9',
-							timestamp: 2,
-							recipientId: 'ife4c0qdb0dopbg538lg14i',
-							status: 50,
-							type: 3,
-							data: 'http://www.publishers.org.uk/_resources/assets/attachment/full/0/2091.pdf',
-							filename: 'An example paper',
-							filesize: 194007,
-						},
-						'2': {
-							id: '1460400827385',
-							senderId: 'ife4c0qdb0dopbg538lg14i',
-							timestamp: 2,
-							recipientId: 'if9ynf7looscygpvakhxs9k9',
-							status: 52,
-							type: 4,
-							data: 'http://www.stephaniequinn.com/Music/Canon.mp3',
-							duration: 10,
-						},
-						'3':{
-							id: '3',
-							senderId: 'if9ynf7looscygpvakhxs9k9',
-							timestamp: 2,
-							recipientId: 'ife4c0qdb0dopbg538lg14i',
-							status: 52,
-							type: 4,
-							data: 'http://www.stephaniequinn.com/Music/Mozart%20-%20Presto.mp3',
-							duration: 10,
-						}
-					}
-				}
-			},
+			conversations: undefined,
 			conversation: undefined,
 			userSession: undefined,
 		}
@@ -99,8 +33,9 @@ class App extends React.Component {
 	}
 	
 	componentWillMount() {
-		this.setState({userSession: this.props.userSession})
-		let conversation = this.state.conversations['ife4c0qdb0dopbg538lg14i'];
+		this.setState({userSession: this.props.userSession});
+		this.setState({conversations: this.props.conversations});
+		let conversation = this.props.conversations['ife4c0qdb0dopbg538lg14i'];
 		this.setState({conversation: conversation});
 		var MONKEY_DEBUG_MODE = false;
 	}
@@ -115,50 +50,30 @@ class App extends React.Component {
 	  	this.setState({conversations: this.state.conversations.concat(conversation)})
 	}
 	
-	handleMessageToSet(message){
+	handleMessageToSet(message) {
 		
 		message.id = Object.keys(this.state.conversations[message.recipientId].messages).length + 1;
 		let conversations = this.state.conversations;
 		conversations[this.state.conversation.id].messages[message.id] = message;
-		this.setState({conversations: conversations});
+		//this.setState({conversations: conversations});
 		console.log(message);
+
+		store.dispatch({
+			type: 'SAVE_MESSAGE',
+			conversations: conversations
+		});
 
 	}
 	
-	updateMessage(){
+	updateMessage() {
 		
 	}
 }
 var userSession = { id:'if9ynf7looscygpvakhxs9k9', name:'Eri', urlAvatar:'https://secure.criptext.com/avatars/avatar_2275.png'};
-render(<App userSession={userSession}/>, document.getElementsByTagName('body')[0]);
 
-/*
-let nodes = [];
-
-const ReactContentRenderer = {
-    unmountAll() {
-        if (nodes.length === 0) {
-            return;
-        }
-        nodes.forEach(node => React.unmountComponentAtNode(node));
-        nodes = [];
-    },
-    render(element, container, callback) {
-        if (container instanceof jQuery) {
-            container = container.get(0);
-        }
-        render(element, container, callback);
-        nodes.push(container);
-    }
-};
-
-function renderApp(userSession){
-	ReactContentRenderer.render(<App userSession={userSession}/>, document.getElementsByTagName('body')[0]);
+function render() {
+	ReactDOM.render(<App userSession={userSession} conversations={store.getState()}/>, document.getElementsByTagName('body')[0]);
 }
 
-$( document ).ready(function() {
-	var userSession = { id:'if9ynf7looscygpvakhxs9k9', name:'Eri', urlAvatar:'https://secure.criptext.com/avatars/avatar_2275.png'};
-	renderApp(userSession);
-	
-});
-*/
+render();
+store.subscribe(render);
