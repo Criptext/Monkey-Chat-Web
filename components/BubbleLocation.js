256210a09c9ff23b1aaf9f562ffa598525ae8461
Pedro Aim <pedro_aim@hotmail.com>
2016-04-22 10:07:02 -0500

    images folder

--- components/BubbleLocation.js
+++ components/BubbleLocation.js
@@ -9,7 +9,7 @@ class BubbleLocation extends React.Component {
 		return (
 			<div className='mky-location-file'>
 				<a className='mky-location-link' href={"https://maps.google.com/maps?q=" + this.props.message.lat + "," + this.props.message.lon + "&amp;z=17"} >
-					<img src="gmap_default.png"></img>
+					<img src="images/gmap_default.png"></img>
 					<div className='mky-location-detail'>
 						<div className='mky-location-name'>
 							<span className='mky-ellipsify'>Ver Mapa</span>
