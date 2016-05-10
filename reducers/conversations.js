17fd4722a431cda0d768389d5f3826408b1e48d1
Erika Perugachi <erikaperugachi@gmail.com>
2016-05-10 17:50:15 -0500

    Add UPDATE_MESSAGE_STATUS action

--- reducers/conversations.js
+++ reducers/conversations.js
@@ -1,4 +1,4 @@
-import { ADD_CONVERSATIONS, ADD_CONVERSATION, UPDATE_CONVERSATION_STATUS, ADD_MESSAGE } from '../actions'
+import { ADD_CONVERSATIONS, ADD_CONVERSATION, UPDATE_CONVERSATION_STATUS, ADD_MESSAGE, UPDATE_MESSAGE_STATUS } from '../actions'
 
 const conversations = (state = {}, action) => {
 	switch(action.type) {
@@ -29,6 +29,14 @@ const conversations = (state = {}, action) => {
 				[conversationId]: conversation(state[conversationId], action)
 			}
 		}
+		
+		case UPDATE_MESSAGE_STATUS: {
+			const conversationId = action.conversationId;
+			return {
+				...state,
+				[conversationId]: conversation(state[conversationId], action)
+			}
+		}
 		default:
 			return state;
 	}
@@ -45,23 +53,53 @@ const conversation = (state, action) => {
 			}
 		}
 		
-		case ADD_MESSAGE:
+		case ADD_MESSAGE: {
 			return {
 				...state,
 				messages: messages(state.messages, action),
 				lastMessage: action.message.id
-			}	
+			}
+		}
+		
+		case UPDATE_MESSAGE_STATUS: {
+			return {
+				...state,
+				messages: messages(state.messages, action)
+			}
+		}
 	}
 	return state;
 }
 
 const messages = (state, action) => {
 	switch (action.type) {
-		case ADD_MESSAGE:
+		case ADD_MESSAGE: {
 			return {
 				...state,
 				[action.message.id]: action.message
 			}
+		}
+		
+		case UPDATE_MESSAGE_STATUS: {
+			const messageId = action.message.id;
+			return {
+				...state,
+				[messageId]: message(state[messageId], action)
+			}
+		}
+	}
+	return state;
+}
+
+const message = (state, action) => {
+	switch (action.type) {
+		case UPDATE_MESSAGE_STATUS: {
+			const messageId = action.message.id;
+			return {
+				...state,
+				data: action.messafe.data
+			}
+		}
 	}
 	return state;
 }
