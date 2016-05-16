14c2d37c178ce38a80929021b242aa2556d07f7f
Raquel Villon <raqevill@espol.edu.ec>
2016-05-16 12:36:52 -0500

    max-height for image preview and position of download button in viewer modal

--- components/ContentViewer.js
+++ components/ContentViewer.js
@@ -3,8 +3,9 @@ import React, { Component } from 'react'
 class ContentViewer extends Component {
 	constructor(props){
 		super(props);
+		this.handleImageLoaded=this.handleImageLoaded.bind(this);
 	}
-	
+
 	render() {
 		return(
 			<div className="mky-viewer-image-container">
@@ -12,13 +13,18 @@ class ContentViewer extends Component {
 					<a href={this.props.message.data} download="file" >
 						<button className="mky-button-download" title="Download">Download</button>
 					</a>
-					<button className="mky-button-download" title="Download" >Print</button>
+					<button className="mky-button-download" title="Print" >Print</button>
 				</div>
-				<div id="file_viewer_image" className="mky-viewer-image">
-					<img  src={this.props.message.data}/>
+				<div id="file_viewer_image" className="mky-viewer-image" >
+					<img id="file_image" src={this.props.message.data} onLoad={this.handleImageLoaded} />
 				</div>
 			</div>
 		)
+	}
+
+	handleImageLoaded(){
+		console.log($('#file_viewer_image').height());
+		$('#file_image').css('max-height',$('#file_viewer_image').height()+'px');
 	}
 
 }
