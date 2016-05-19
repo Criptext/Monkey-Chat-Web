d947bd84a01bf9b764e31eaa5f12844a7d037a76
Pedro Aim <pedro_aim@hotmail.com>
2016-05-19 10:17:04 -0500

    commented location input

--- components/InputMenu.js
+++ components/InputMenu.js
@@ -8,6 +8,7 @@ class InputMenu extends React.Component {
 	render() {
 		return (
 			<div id="menu-bubble" className="menu-bubble" style={{display : this.props.visible ? "block" : "none"}}>
+				{/*<div className="menu-bubble-item" onClick={this.props.enableGeoInput}><i id="mnk-menu-location-icon" className="demo-icon mky-location">&#xe815;</i><p>Send Location</p></div>*/}
 				<div className="menu-bubble-item" onClick={this.props.handleAttach}><i id="mnk-menu-attach-icon" className="demo-icon mky-attach">&#xe80a;</i> <p>Attach File</p></div>
 				<div id="layer-menu" onClick={this.props.toggleVisibility} ></div>
 			</div>
